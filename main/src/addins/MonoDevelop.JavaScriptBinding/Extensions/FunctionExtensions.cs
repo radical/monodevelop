70f88ac4cfddbc7ff3f78f8820fee2edb349d9c7
Harsimran Bath <harsimranb@outlook.com>
2014-05-02 22:31:32 -0700

    [JS] Initial import of new JS addin
    Includes:
    
    * The Jurassic JavaScript project from jurassic.codeplex.com, modified to
      - Add FunctionStatement support in parser.
      - Changed internal to public.
      - Fixed positions of some statements in parsing.
      - Add simple error reporting
    
    * An updated SyntaxMode.xml based on SyntaxMode from Html Project.
    * Empty JS File Template
    * JavaScript Language Binding, Parser, Parsed Document, Foldings, Errors,
      and other helpful classes/notes.
    * Document Outline Support with Icons.

---
+++ main/src/addins/MonoDevelop.JavaScriptBinding/Extensions/FunctionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoDevelop.JavaScript.Extensions
+{
+	static class FunctionExtensions
+	{
+		public static string BuildFunctionSignature (this Jurassic.Compiler.FunctionExpression function)
+		{
+			return buildFunctionSignature (function.FunctionName, function.ArgumentNames);
+		}
+
+		public static string BuildFunctionSignature (this Jurassic.Compiler.FunctionStatement function)
+		{
+			return buildFunctionSignature (function.FunctionName, function.ArgumentNames);
+		}
+
+		static string buildFunctionSignature (string functionName, IList<string> argumentNames)
+		{
+			if (string.IsNullOrWhiteSpace (functionName))
+				functionName = "function"; // for inline functions, callbacks, delegates, etc.
+
+			var builder = new StringBuilder (functionName);
+
+			builder.Append (" (");
+			if (argumentNames.Count > 0) {
+				for (int i = 0; i < argumentNames.Count; i++) {
+					var currentArgument = argumentNames [i];
+					if (i + 1 != argumentNames.Count) {
+						builder.Append (string.Concat (currentArgument, ", "));
+					} else {
+						builder.Append (string.Concat (currentArgument));
+					}
+				}
+			}
+			builder.Append (")");
+
+			return builder.ToString ();
+		}
+	}
+}
